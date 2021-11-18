6b0ef81744124145bd0d60a4ea07e581410fc6a6
alexandr <alexzhalnerchikz@mail.ru>
2021-11-18 14:58:35 +0300

    init commit

---
+++ src/test/java/com/bsu/tpbackend/TpBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bsu.tpbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
